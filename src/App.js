842de76241e52adf452dc98c7eed647f0efcd2e1
Adam <chillcoders@gmail.com>
2022-01-26 11:56:13 -0500

    usestate

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "./theme";
 import { GlobalStyles } from "./global";
